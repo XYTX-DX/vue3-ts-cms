d02d02d06405bc3150ff088c8aaa94314ef89494
XYTX-DX <2392597983@qq.com>
2021-09-06 17:09:19 +0800

    feat(main-nav-menu): main-nav-menu

---
+++ src/components/nav-menu/src/nav-menu.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="nav-menu">
+    <div class="logo">
+      <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
+      <span class="title">Vue3+TS</span>
+    </div>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template #title>分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <template #title>导航二</template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <template #title>导航三</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <template #title>导航四</template>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-menu {
+  height: 100%;
+  background-color: #001529;
+
+  .logo {
+    display: flex;
+    height: 28px;
+    padding: 12px 10px 8px 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    .img {
+      height: 100%;
+      margin: 0 10px;
+    }
+
+    .title {
+      font-size: 16px;
+      font-weight: 700;
+      color: white;
+    }
+  }
+
+  .el-menu {
+    border-right: none;
+  }
+
+  // 目录
+  .el-submenu {
+    background-color: #001529 !important;
+    // 二级菜单 ( 默认背景 )
+    .el-menu-item {
+      padding-left: 50px !important;
+      background-color: #0c2135 !important;
+    }
+  }
+
+  ::v-deep .el-submenu__title {
+    background-color: #001529 !important;
+  }
+
+  // hover 高亮
+  .el-menu-item:hover {
+    color: #fff !important; // 菜单
+  }
+
+  .el-menu-item.is-active {
+    color: #fff !important;
+    background-color: #0a60bd !important;
+  }
+}
+
+.el-menu-vertical:not(.el-menu--collapse) {
+  width: 100%;
+  height: calc(100% - 48px);
+}
+</style>
